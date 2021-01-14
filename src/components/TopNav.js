2eae59e9cd39e35374ad1cf722420bc45a221560
itsjustbolu <boluowolana@gmail.com>
2021-01-13 18:41:46 -0600

    nav done

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -1,16 +1,42 @@
 import React from "react";
+import styled from "styled-components";
+
+const NavContainer = styled.div`
+  background-color: gray;
+  padding: 2em;
+`;
+
+const LogoPlaceholder = styled.div`
+
+`
+
+const Navlinks = styled.a`
+  font-size: 2em;
+  margin: 2em;
+  padding: 2em;
+`;
 
 function TopNavbar() {
   return (
-    <div class="nav-container">
-      <div class="nav-links">
-        <a href="about">About</a>
-        <a href="/blog">Blog</a>
-        <a href="/projects">Projects</a>
-        <a href="/contact">Contact</a>
+    <NavContainer>
+      
+      <div class="nav-container">
+        <div class="nav-links">
+          <Navlinks>
+            <a href="/about">About</a>
+          </Navlinks>
+          <Navlinks>
+            <a href="/blog">Blog</a>
+          </Navlinks>
+          <Navlinks>
+            <a href="/contact">Contact</a>
+          </Navlinks>
+          <Navlinks>
+            <a href="/projects">Projects</a>
+          </Navlinks>
+        </div>
       </div>
-    </div>
-    
+    </NavContainer>
   );
 }
 
