0f77d86d7ef5ef44dba6ae244ab0881e2a510cde
itsjustbolu <boluowolana@gmail.com>
2021-01-17 14:49:33 -0600

    bug fixes

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -41,6 +41,7 @@ const Content = styled.div`
   font-family: "Open Sans", sans-serif;
   font-size: 1em;
   margin: 2em 2.5em;
+  line-height: 2em;
 `;
 
 const Certifications = styled.div`
@@ -64,33 +65,35 @@ export class AboutPage extends Component {
           <p>
             I'm a former Petroleum Geologist and Geophysicist turned Cloud &
             DevOps Engineer that loves building, deploying and maintaining cloud
-            resources to ensure maximum uptime, fault tolerance and high
-            availability.
+            resources to ensure maximum uptime, fault-tolerance and
+            high-availability.
           </p>
           <p>
-            I have learned a lot as a Cloud Engineer, and I continuously develop
-            and challenge myself to constantly absorb relevant emerging
-            technologies. Using programming languages like Python, Javascript
-            and Bash, as well as tools Cloudformation and Terraform enables
-            automation.
+            My favorite devops tools are Chef, CloudFormation, Puppet and
+            Terraform. I'm also very familiar with version control systems like
+            CodeCommit, Git, Github and CI/CD tools like CodeBuild, CodeDeploy,
+            CodePipeline and Jenkins. As far as cloud technologies, I feel at
+            home with AWS, however I have dabbled in Google Cloud and Azure for
+            some projects. I continuously strive to keep abreast of relevant
+            emerging technologies and tools that could be more efficient and/or
+            provide better workflows.
           </p>
           <p>
             I write a{" "}
             <strong>
               <a href="/blog">blog</a>
             </strong>{" "}
-            sharing some of my learnings and tips as an Engineer, and tend to
-            build pet{" "}
+            sharing some of my learnings and also build small{" "}
             <strong>
               <a href="/projects">projects</a>
             </strong>{" "}
-            in my spare time to keep my programming and software engineering
-            skills sharp.
+            in my spare time to keep my software engineering skills sharp.
           </p>
           <p>
             I am <strong>3x AWS Certified</strong>, and my specialties include
-            AWS, Automation via IaC, CI/CD, Cloud Computing, Container Services,
-            DevOps, Git, Python and Serverless Architecture.{" "}
+            AWS, Automation via IaC, Bash, CI/CD, Cloud Computing, Container
+            Services, DevOps, Git, Linux, Python Serverless Architecture and
+            System Administration.{" "}
           </p>
           <p>
             If you've got any questions about my journey or simply want to say
